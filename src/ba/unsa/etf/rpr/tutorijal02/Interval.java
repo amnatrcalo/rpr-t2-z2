e73c2ad5afc134ff9083eaa8ac7c825d3c497d26
Amna Trcalo <atrcalo1@etf.unsa.ba>
2020-11-02 19:43:28 +0100

    konstruktor i isnull

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -1,6 +1,27 @@
 package ba.unsa.etf.rpr.tutorijal02;
 
 public class Interval {
+private double pocetnaTacka;
+private double krajnjaTacka;
+private boolean pocetnaPripada;
+private boolean krajnjaPripada;
+public Interval(double pocetnaTacka, double krajnjaTacka, boolean pocetnaPripada, boolean krajnjaPripada) throws Exception{
+    if(pocetnaTacka>krajnjaTacka) throw new IllegalArgumentException("Neispravni parametri.");
+    this.pocetnaTacka=pocetnaTacka;
+    this.krajnjaTacka=krajnjaTacka;
+    this.pocetnaPripada=pocetnaPripada;
+    this.krajnjaPripada=krajnjaPripada;
+}
 
+public Interval(){
+    pocetnaTacka=0;
+    krajnjaTacka=0;
+    pocetnaPripada=false;
+    krajnjaPripada=false;
+}
+public boolean isNull(){
+    if(this.pocetnaTacka==0 && this.krajnjaTacka==0 && this.pocetnaPripada==false && this.krajnjaPripada==false) return false;
+    else return true;
+}
 
 }
